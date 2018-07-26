ef7dfd59dbcd4466c24533d4a2481a691e945b6b
Suraj Kanojiya <suraj.kanojiya@biz2credit.org>
2018-07-26 12:16:25 +0530

    Query changed

--- src/main/java/com/pollapp/poll/repository/VoteRepository.java
+++ src/main/java/com/pollapp/poll/repository/VoteRepository.java
@@ -17,10 +17,10 @@ import com.pollapp.poll.model.Vote;
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 	
-	@Query("SELECT NEW com.example.polls.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id in :pollIds GROUP BY v.choice.id")
+	@Query("SELECT NEW com.pollapp.poll.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id in :pollIds GROUP BY v.choice.id")
     List<ChoiceVoteCount> countByPollIdInGroupByChoiceId(@Param("pollIds") List<Long> pollIds);
 
-    @Query("SELECT NEW com.example.polls.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id = :pollId GROUP BY v.choice.id")
+    @Query("SELECT NEW com.pollapp.poll.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id = :pollId GROUP BY v.choice.id")
     List<ChoiceVoteCount> countByPollIdGroupByChoiceId(@Param("pollId") Long pollId);
 
     @Query("SELECT v FROM Vote v where v.user.id = :userId and v.poll.id in :pollIds")
